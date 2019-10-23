454ce171e213848b98754923e5032de4a5a5153e
Nathalie A. Vladis <nathalievladis@gmail.com>
2019-10-23 15:48:05 -0400

    Created packages installation file

---
+++ install.R
@@ -0,0 +1,10 @@
+# Data analysis packages
+library(dplyr)
+library(ggplot2)
+
+# The package containing our dataset
+install.packages('gapminder')
+library(gapminder)
+
+# Palletes
+install.packages(RColorBrewer)
